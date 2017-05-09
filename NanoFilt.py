c7f77dbc658122cc7e07bdf1694c057f5f22da64
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-05-09 21:43:14 +0200

    added read trimming

--- NanoFilt.py
+++ NanoFilt.py
@@ -9,12 +9,14 @@ def getArgs():
     parser = argparse.ArgumentParser(description="Perform quality and or length filtering of Nanopore fastq data on stdin.")
     parser.add_argument("-q", "--quality", help="Filter on a minimum average read quality score", default=0, type=int)
     parser.add_argument("-l", "--length", help="Filter on a minimum read length", default=0, type=int)
+    parser.add_argument("--headcrop", help="Trim n nucleotides from start of read", default=0, type=int)
+    parser.add_argument("--tailcrop", help="Trim n nucleotides from end of read", default=0, type=int)
     return parser.parse_args()
 
 def filterstream(fq):
     for record in SeqIO.parse(fq, "fastq"):
         if aveQualFastq(record.letter_annotations["phred_quality"]) > args.quality and len(record) > args.length:
-            print(record.format("fastq"), end="")
+            print(record[args.headcrop:args.tailcrop].format("fastq"), end="")
 
 if __name__ == "__main__":
 	args = getArgs()
