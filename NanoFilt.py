d449c67fabaebc942fe9beb54f33c968a3aa48a2
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-05-18 18:49:38 +0200

    updated defaults

--- NanoFilt.py
+++ NanoFilt.py
@@ -14,8 +14,8 @@ def getArgs():
     parser = argparse.ArgumentParser(description="Perform quality and or length filtering of Nanopore fastq data on stdin.")
     parser.add_argument("-q", "--quality", help="Filter on a minimum average read quality score", default=0, type=int)
     parser.add_argument("-l", "--length", help="Filter on a minimum read length", default=0, type=int)
-    parser.add_argument("--headcrop", help="Trim n nucleotides from start of read", default=0, type=int)
-    parser.add_argument("--tailcrop", help="Trim n nucleotides from end of read", default=0, type=int)
+    parser.add_argument("--headcrop", help="Trim n nucleotides from start of read", default=None, type=int)
+    parser.add_argument("--tailcrop", help="Trim n nucleotides from end of read", default=None, type=int)
     return parser.parse_args()
 
 def filterstream(fq):
