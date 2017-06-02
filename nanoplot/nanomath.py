93d6062e4e628753caf2cc804a41c6506403dc87
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-02 14:01:17 +0200

    moved some calculation functions

---
+++ nanoplot/nanomath.py
@@ -0,0 +1,18 @@
+# wdecoster
+import numpy as np
+import pandas as pd
+
+
+def getN50(a):
+	'''
+	Calculator function: Get read N50.
+	Based on https://github.com/PapenfussLab/Mungo/blob/master/bin/fasta_stats.py
+	'''
+	return a[np.where(np.cumsum(a)>=0.5*np.sum(a))[0][0]]
+
+
+def removeLengthOutliers(df, columnname):
+	'''
+    Calculation function: Remove records with length-outliers
+    '''
+	return df[df[columnname] < (np.median(df[columnname]) + 3 * np.std(df[columnname]))]
