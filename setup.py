bcbe77b46b432d048255272dbcdca80dd91ab198
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-03 19:03:12 +0200

    made simple setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='NanoPlot',
+    version='0.9.0',
+    description='Plotting suit for Oxford Nanopore sequencing data and alignments',
+    long_description='Plotting suit for Oxford Nanopore sequencing data, reading data from fastq and bam files.',
+    url='https://github.com/wdecoster/NanoPlot',
+    author='Wouter De Coster',
+    author_email='decosterwouter@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='nanopore sequencing plotting quality control',
+    packages=find_packages(),
+    install_requires=['biopython',
+        'pysam>0.10.0.0',
+        'pandas',
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'python-dateutil',
+        'seaborn'
+        ],
+    package_data={'NanoPlot': []},
+    package_dir={'nanoplot': 'nanoplot'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'NanoPlot=nanoplot.NanoPlot:main',
+        ],
+    },
+)
