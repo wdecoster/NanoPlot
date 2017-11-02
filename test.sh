58cafd826c7f4454a56aba1b6284e9a1b9b1a19f
Wouter <decosterwouter@gmail.com>
2017-11-02 18:02:32 +0100

    running a bash test script which downloads test data

---
+++ test.sh
@@ -0,0 +1,31 @@
+set -e
+
+git clone https://github.com/wdecoster/nanotest.git
+
+NanoPlot -h
+NanoPlot --listcolors
+echo ""
+echo ""
+echo ""
+echo "testing bam:"
+NanoPlot --bam nanotest/alignment.bam --verbose
+echo ""
+echo ""
+echo ""
+echo "testing summary:"
+NanoPlot --summary nanotest/sequencing_summary.txt --loglength --verbose
+echo ""
+echo ""
+echo ""
+echo "testing fastq rich:"
+NanoPlot --fastq_rich nanotest/reads.fastq.gz --report --verbose --downsample 800
+echo ""
+echo ""
+echo ""
+echo "testing fastq minimal:"
+NanoPlot --fastq_minimal nanotest/reads.fastq.gz --store --verbose --plot dot
+echo ""
+echo ""
+echo ""
+echo "testing fastq plain:"
+NanoPlot --fastq nanotest/reads.fastq.gz --verbose --minqual 4 --color red
