208f80c42ecfd02f60d623fa23f49d09146ba8ce
wdecoster <decosterwouter@gmail.com>
2017-11-24 18:34:21 +0100

    reset path after processing all barcodes

--- nanoplot/NanoPlot.py
+++ nanoplot/NanoPlot.py
@@ -76,6 +76,7 @@ def main():
                 plots.extend(
                     make_plots(dfbarc, settings)
                 )
+            settings["path"] = path.join(args.outdir, args.prefix)
         else:
             plots = make_plots(datadf, settings)
         make_report(plots, settings["path"], logfile)
