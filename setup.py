9a9748edaab7d8e8092bed69737c86c4f6ad2353
wdecoster <decosterwouter@gmail.com>
2018-09-17 21:21:18 +0200

    make dependent on latest nanoplotter
    prevents https://github.com/wdecoster/NanoPlot/issues/90

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
                       'scipy',
                       'python-dateutil',
                       'seaborn',
-                      'nanoplotter>=0.44.0',
+                      'nanoplotter>=0.44.1',
                       'nanoget>=1.7.2',
                       'nanomath>=0.21.0'
                       ],
