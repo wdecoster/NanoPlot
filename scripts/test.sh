91f01a08bca7a19d264d9ac1d3808f4a1177f113
wdecoster <decosterwouter@gmail.com>
2021-04-01 10:23:11 +0200

    changing test.sh to check if nanotest already cloned
    and use a single output dir

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 set -ev
 
-git clone https://github.com/wdecoster/nanotest.git
+if [ -d "nanotest" ]; then
+    echo "nanotest already cloned"
+else
+    git clone https://github.com/wdecoster/nanotest.git
+fi
 
 NanoPlot -h
 # NanoPlot --listcolors
@@ -8,37 +12,37 @@ echo ""
 echo ""
 echo ""
 echo "testing bam:"
-NanoPlot --bam nanotest/alignment.bam --verbose
+NanoPlot --bam nanotest/alignment.bam --verbose -o tests
 echo ""
 echo ""
 echo ""
 echo "testing bam without supplementary alignments:"
-NanoPlot --bam nanotest/alignment.bam --verbose --no_supplementary
+NanoPlot --bam nanotest/alignment.bam --verbose --no_supplementary -o tests
 echo ""
 echo ""
 echo ""
 echo "testing summary:"
-NanoPlot --summary nanotest/sequencing_summary.txt --loglength --verbose
+NanoPlot --summary nanotest/sequencing_summary.txt --loglength --verbose -o tests
 echo ""
 echo ""
 echo ""
 echo "testing fastq rich:"
-NanoPlot --fastq_rich nanotest/reads.fastq.gz --verbose --downsample 800
+NanoPlot --fastq_rich nanotest/reads.fastq.gz --verbose --downsample 800 -o tests
 echo ""
 echo ""
 echo ""
 echo "testing fastq minimal:"
-NanoPlot --fastq_minimal nanotest/reads.fastq.gz --store --verbose --plot dot
+NanoPlot --fastq_minimal nanotest/reads.fastq.gz --store --verbose --plot dot -o tests
 echo ""
 echo ""
 echo ""
 echo "testing fastq plain:"
-NanoPlot --fastq nanotest/reads.fastq.gz --verbose --minqual 4 --color red
+NanoPlot --fastq nanotest/reads.fastq.gz --verbose --minqual 4 --color red -o tests
 echo ""
 echo ""
 echo ""
 echo "testing fasta:"
-NanoPlot --fasta nanotest/reads.fa.gz --verbose --maxlength 35000
+NanoPlot --fasta nanotest/reads.fa.gz --verbose --maxlength 35000 -o tests
 echo ""
 echo ""
 echo ""
