2882849aa90a22b41e3dd0f62af123bb838e7443
Christian Brueffer <christian@brueffer.io>
2023-05-25 11:21:04 +0200

    Update license; missed in 80caf99a88d80dc61a30e975a1aa4375a93a7be0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
