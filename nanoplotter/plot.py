416517bd3dc0d94ff9bde0092557fb13f159830f
Austin Marshall <agmarshall@houstonmethodist.org>
2025-07-23 08:26:53 -0500

    fix plot.py to work with kaleido v1

--- nanoplotter/plot.py
+++ nanoplotter/plot.py
@@ -3,8 +3,14 @@ from base64 import b64encode
 from io import BytesIO
 from urllib.parse import quote as urlquote
 import sys
-from kaleido.scopes.plotly import PlotlyScope
 import logging
+
+# bring in kaleido and ensure Chrome is installed
+import kaleido
+# this will download a small headless Chrome build the first time you run it
+kaleido.get_chrome_sync()
+
+from kaleido import write_fig_sync
 
 
 class Plot(object):
@@ -27,12 +33,10 @@ class Plot(object):
             return self.encode1()
 
     def encode1(self):
-        """Return the base64 encoding of the figure file and insert in html image tag."""
         data_uri = b64encode(open(self.path, "rb").read()).decode("utf-8").replace("\n", "")
         return '<img src="data:image/png;base64,{0}">'.format(data_uri)
 
     def encode2(self):
-        """Return the base64 encoding of the fig attribute and insert in html image tag."""
         buf = BytesIO()
         self.fig.savefig(buf, format="png", bbox_inches="tight", dpi=100)
         buf.seek(0)
@@ -40,7 +44,7 @@ class Plot(object):
         return '<img src="data:image/png;base64,{0}">'.format(urlquote(string))
 
     def save(self, settings):
-        if not(self.only_report):
+        if not self.only_report:
             if self.html:
                 with open(self.path, "w") as html_out:
                     html_out.write(self.html)
@@ -52,12 +56,10 @@ class Plot(object):
                         p = os.path.splitext(self.path)[0] + ".png"
                         if os.path.exists(p):
                             os.remove(p)
-
                         logging.warning("No static plots are saved due to some kaleido problem:")
                         logging.warning(e)
 
             elif self.fig:
-                # if settings["format"] is a list, save the figure in all formats
                 if isinstance(settings["format"], list):
                     for fmt in settings["format"]:
                         self.fig.savefig(
@@ -66,7 +68,11 @@ class Plot(object):
                             bbox_inches="tight",
                         )
                 else:
-                    self.fig.savefig(fname=self.path, format=settings["format"], bbox_inches="tight")
+                    self.fig.savefig(
+                        fname=self.path,
+                        format=settings["format"],
+                        bbox_inches="tight",
+                    )
             else:
                 sys.exit("No method to save plot object: no html or fig defined.")
 
@@ -77,9 +83,14 @@ class Plot(object):
             sys.stderr.write(".show not implemented for Plot instance without fig attribute!")
 
     def save_static(self, figformat):
-        scope = PlotlyScope()
-        with open(self.path.replace("html", figformat), "wb") as f:
-            f.write(scope.transform(self.fig, format=figformat))
-            logging.info(
-                f"Saved {self.path.replace('.html', '')}  as {figformat} (or png for --legacy)"
-            )
+        """
+        Export a Plotly figure via Kaleido v1’s write_fig_sync.
+        """
+        output_path = self.path.replace(".html", f".{figformat}")
+        try:
+            write_fig_sync(self.fig, path=output_path)
+            logging.info(f"Saved {output_path} as {figformat}")
+        except Exception as e:
+            logging.warning("No static plots are saved due to some kaleido problem:")
+            logging.warning(e)
+
